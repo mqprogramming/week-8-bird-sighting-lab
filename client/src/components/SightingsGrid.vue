84e4198c66c1ca154bf8b999994814f92f32def9
mqprogramming <matt.qgley@gmail.com>
2020-04-07 14:55:15 +0100

    first commit

---
+++ client/src/components/SightingsGrid.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+	<div id="sightingsGrid">
+		<sighting v-for="sighting in sightings" :sighting="sighting" />
+	</div>
+</template>
+
+<script>
+import Sighting from './Sighting';
+import SightingService from '../services/SightingService.js';
+import { eventBus } from '../main';
+export default {
+	name: "sightings-grid",
+	data () {
+    return {
+      sightings: []
+    }
+  },
+	components: {
+		'sighting': Sighting
+	},
+	mounted(){
+		this.fetchData();
+		
+		eventBus.$on('add-sighting', (newSighting) => {
+			this.sightings.push(newSighting)
+		})
+  },
+  methods: {
+    fetchData(){
+      SightingService.getSightings()
+      .then(sightings => this.sightings = sightings);
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+#sightingsGrid {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+}
+
+h2 {
+	padding: 0;
+	margin: 0;
+}
+
+
+</style>
